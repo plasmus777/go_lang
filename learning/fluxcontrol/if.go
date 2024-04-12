eb395bbedf1ec13ac0ba446caf27c40b532df8ac
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-04-12 18:09:49 -0300

    If-else

--- learning/fluxcontrol/if.go
+++ learning/fluxcontrol/if.go
@@ -3,5 +3,37 @@ package fluxcontrol
 import "fmt"
 
 func If() {
-	fmt.Println()
+
+	value := 999.00
+	var valuePlus float64
+
+	valuePlus = value
+
+	/*
+		<
+		>
+		<=
+		>=
+	*/
+
+	addExtra := true
+
+	if addExtra {
+		fmt.Println("Adding 100 to the total value.")
+		if value <= 1000 {
+			valuePlus += 100
+		}
+	} else {
+		fmt.Println("The total value will remain intact.")
+	}
+
+	if valuePlus > 1000 {
+		fmt.Println("The total value is greater than 1000.")
+	} else if valuePlus == 1000 {
+		fmt.Println("The total value is equal to 1000.")
+	} else if valuePlus < 1000 {
+		fmt.Println("The total value is lesser than 1000.")
+	}
+
+	fmt.Println(valuePlus)
 }
